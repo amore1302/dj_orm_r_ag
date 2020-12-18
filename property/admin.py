151c3e083b2d57c2dff8b60c8fa98cd7273c9615
amore1302 <amore1302@mail.ru>
2020-12-18 22:13:34 +0300

    add model Claim, AdminClaim

--- property/admin.py
+++ property/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
+from .models import Flat, Claim
 
-from .models import Flat
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town','address','owner',)
-    list_filter = ('new_building','rooms_number','has_balcony',)
-    list_display = ('address','price','new_building','construction_year','town',)
+    search_fields = ('town', 'address', 'owner',)
+    list_filter = ('new_building', 'rooms_number', 'has_balcony',)
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town',)
     list_editable = ('new_building',)
     readonly_fields = ["created_at"]
 
-admin.site.register(Flat,FlatAdmin)
+class ClaimAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user', 'flat',)
+
+
+admin.site.register(Flat, FlatAdmin)
+admin.site.register(Claim, ClaimAdmin)
+
+
+
+
+
--- property/models.py
+++ property/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 class Flat(models.Model):
     owner = models.CharField("ФИО владельца", max_length=200)
     owners_phonenumber = models.CharField("Номер владельца", max_length=20)
     created_at = models.DateTimeField("Когда создано объявление", default=timezone.now, db_index=True)
-    
+
     description = models.TextField("Текст объявления", blank=True)
     price = models.IntegerField("Цена квартиры", db_index=True)
 
@@ -25,3 +25,10 @@ class Flat(models.Model):
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
+
+class Claim(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, null=False,blank=False, verbose_name='Кто пожаловался')
+    flat = models.ForeignKey(Flat, on_delete=models.PROTECT, null=False,blank=False,
+                            verbose_name='Квартира, на которую жалуются')
+    description = models.TextField(blank=True, verbose_name='Текст жалобы')
+
